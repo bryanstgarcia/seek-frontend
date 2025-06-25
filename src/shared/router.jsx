6813e026f7d443f566a24fd360d3e72623f8894c
Bryan Garcia <bryan1997st@gmail.com>
2025-06-25 03:22:39 -0500

    Update router configuration to include authentication and task management routes
    - Added routes for LogIn and SignUp components to facilitate user authentication.
    - Introduced a route for the Tasks component to manage task-related functionalities.
    - Updated the existing login route to use the correct LogIn component.

--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { createBrowserRouter } from "react-router";
 import App from "./views/App.jsx";
 import { Home } from "./views/home/Home.jsx";
+import { LogIn } from "../authentication/views/LogIn/LogIn.jsx";
+import { SignUp } from "../authentication/views/SignUp/SignUp.jsx";
+import { Tasks } from "../tasks/views/Tasks/Tasks.jsx";
+
 
 export const router = createBrowserRouter([
     {
@@ -14,9 +18,16 @@ export const router = createBrowserRouter([
             },
             {
                 path: "login",
-                element: <Login />,
+                element: <LogIn />,
             },
-            
+            {
+                path: "signup",
+                element: <SignUp />,
+            },
+            {
+                path: "tasks",
+                element: <Tasks />,
+            }
         ],
     },
 ]);
