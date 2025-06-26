a99d74d91693ee9e4e2c76068c7e9b936205663b
Bryan Garcia <bryan1997st@gmail.com>
2025-06-26 16:05:02 -0500

    Refactor TasksBoard component to simplify height handling
    - Removed height prop from TasksBoard component for cleaner API.
    - Updated height styling to use a default value of "auto" for improved layout consistency across different screen sizes.

--- src/tasks/views/components/TasksBoard/TasksBoard.jsx
+++ src/tasks/views/components/TasksBoard/TasksBoard.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Box } from "@mui/material";
 import { styles } from "./styles";
 
-export const TasksBoard = ({ height, children }) => {
+export const TasksBoard = ({ children }) => {
     return (
         <Box
-            sx={{...styles.mainBox, height}}
+            sx={{...styles.mainBox, height: {
+                sm: "auto",
+                
+            }}}
         >
             { children }
         </Box>
