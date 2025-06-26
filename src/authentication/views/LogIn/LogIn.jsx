7dce2523ef29aac7057c242654cd2d7e084878cc
Bryan Garcia <bryan1997st@gmail.com>
2025-06-26 16:03:20 -0500

    Refactor LogIn component and add styles for improved UI and user feedback
    - Updated LogIn component to include success and error alerts for user feedback during login.
    - Refactored handleSubmit function to manage login logic asynchronously.
    - Introduced styles.js for centralized styling of LogIn component, enhancing maintainability and readability.
    - Improved layout and structure of the LogIn form for better user experience.

--- src/authentication/views/LogIn/LogIn.jsx
+++ src/authentication/views/LogIn/LogIn.jsx
@@ -1,45 +1,40 @@
-import { Box, TextField, Button, Typography, useTheme} from '@mui/material';
-import React, { useState } from 'react';
+import { Box, TextField, Button, Typography, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
+import Alert from "@mui/material/Alert";
+import { styles } from "./styles";
+import { useNavigate } from "react-router";
 
 export const LogIn = () => {
     const theme = useTheme();
-
+    const navigate = useNavigate()
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [success, setSuccess] = useState(null)
+    const { loading, error, login } = useLogin();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         // Handle login logic here
+        const {token, userId } = await login(email, password);
+        if (token && userId) {
+            setSuccess(true)
+            setTimeout(() => {
+                navigate("/tasks")
+                setSuccess(null)
+            }, 2000)
+        }
     };
 
     return (
         <Box
-            sx={{
-                minHeight: "90vh",
-                mx: 'auto',
-                p: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                gap: 3,
-                boxShadow: 3,
-                borderRadius: 2,
-            }}
+            sx={styles.mainBox}
         >
             <Box
                 component="form"
                 onSubmit={handleSubmit}
                 sx={{
-                    maxWidth: 600,
-                    width: "100%",
-                    mx: 'auto',
-                    p: 8,
-                    pt: 6,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: 3,
-                    boxShadow: 3,
-                    borderRadius: 2,
+                    ...styles.form,
                     backgroundColor: theme.palette.neutral.black,
                 }}
             >
@@ -47,8 +42,15 @@ export const LogIn = () => {
                     Log In 🥳
                 </Typography>
                 <Typography component="p" variant="h6" fontWeight={400} mb={2}>
-                    Conquer your day—log in now and take control of your goals, one task at a time
+                    Conquer your day—log in now and take control of your goals,
+                    one task at a time
                 </Typography>
+                {error ? (
+                    <Alert severity="error">There is an error: {error}</Alert>
+                ) : null}
+                {success && !error ? (
+                    <Alert severity="success">Welcome! ✅</Alert>
+                ) : null}
                 <TextField
                     label="Email"
                     type="email"
@@ -67,10 +69,16 @@ export const LogIn = () => {
                     fullWidth
                     autoComplete="current-password"
                 />
-                <Button type="submit" variant="contained" color="primary" size="large">
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    loading={loading}
+                >
                     Log In
                 </Button>
             </Box>
         </Box>
     );
-}
+};
---
+++ src/authentication/views/LogIn/styles.js
@@ -0,0 +1,26 @@
+export const styles = {
+    mainBox: {
+        minHeight: "90vh",
+        mx: "auto",
+        p: 4,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: 3,
+        boxShadow: 3,
+        borderRadius: 2,
+    },
+    form: {
+        maxWidth: 600,
+        width: "100%",
+        mx: "auto",
+        p: 8,
+        pt: 6,
+        display: "flex",
+        flexDirection: "column",
+        gap: 3,
+        boxShadow: 3,
+        borderRadius: 2,
+        
+    }
+}
