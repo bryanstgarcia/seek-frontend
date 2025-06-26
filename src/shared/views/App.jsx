cd79c1b1c6d047d55b785a47ee8719c463016905
Bryan Garcia <bryan1997st@gmail.com>
2025-06-26 16:07:12 -0500

    Integrate authentication and global state management into App component
    - Added AuthContextManager to manage user authentication state.
    - Wrapped the Navbar and Outlet components with GlobalState for centralized state management.
    - Ensured consistent formatting and structure in the App component for improved readability.

--- src/shared/views/App.jsx
+++ src/shared/views/App.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import './App.css'
-import { Outlet } from 'react-router'
-import { Navbar } from './components/Navbar/Navbar.jsx'
-import { Footer } from './components/Footer/Footer.jsx'
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProviderComponent } from './theme.jsx'
+import React from "react";
+import "./App.css";
+import { Outlet } from "react-router";
+import { Navbar } from "./components/Navbar/Navbar.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProviderComponent } from "./theme.jsx";
+import { AuthContextManager } from "../../authentication/state/AuthContext.jsx";
+import { GlobalState } from "../state/GlobalState.jsx";
 
 function App() {
-  return (
-    <>
-    <ThemeProviderComponent>
-      <CssBaseline />
-      <Navbar />
-        { <Outlet />}
-      {/* <Footer /> */}
-    </ThemeProviderComponent>
-    </>
-  )
+    return (
+        <>
+            <ThemeProviderComponent>
+                <CssBaseline />
+                <AuthContextManager>
+                    <GlobalState>
+                        <Navbar />
+                        {<Outlet />}
+                        {/* <Footer /> */}
+                    </GlobalState>
+                </AuthContextManager>
+            </ThemeProviderComponent>
+        </>
+    );
 }
 
-export default App
+export default App;
