740d6ffc967fa503d90731505b4a6900cb311c26
Bryan Garcia <bryan1997st@gmail.com>
2025-06-26 16:01:23 -0500

    Implement AuthContextManager for user authentication state management
    - Added useState to manage user authentication state, including token and userId.
    - Implemented authenticate function to update state and store credentials in localStorage.
    - Initialized state from localStorage on component mount to persist user session.
    - Provided userAuth and authenticate function through AuthContext for consumption in other components.

--- src/authentication/state/AuthContext.jsx
+++ src/authentication/state/AuthContext.jsx
@@ -1,10 +1,32 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 const AuthContext = createContext()
 
-export const AuthContextManager = ({children}) => {
-    // create the state here
-    return <AuthContext.Provider>
-        { children }
-    </AuthContext.Provider>
+export const AuthContextManager = ({ children }) => {
+    const [userAuth, setUserAuth] = useState({token: null, userId: null})
+    
+    const authenticate = ({ userId = null, token = null}) => {
+        setUserAuth({ userId, token })
+        localStorage.setItem("token", token)
+        localStorage.setItem("userId", userId)
+    }
+    
+    useState(() => {
+        const token = localStorage.getItem("token")
+        const userId = localStorage.getItem("userId")
+        if (token  && userId) {
+            setUserAuth({
+                userId,
+                token
+            })
+        }
+    }, [])
+    
+    return (
+        <AuthContext.Provider value={{ userAuth, authenticate }}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
+
+export { AuthContext }
